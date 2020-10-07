dfd56275e1364d7423d409a6de27f448d70521af
Michael Colonel <pmkclnl@yandex.ru>
2020-10-07 19:55:47 +0300

    cont9

--- PlmDrr2/plastimatch_slicer_drr.cxx
+++ PlmDrr2/plastimatch_slicer_drr.cxx
@@ -46,7 +46,6 @@
 #include <itkRescaleIntensityImageFilter.h>
 #include <itkInvertIntensityImageFilter.h>
 #include <itkCastImageFilter.h>
-#include <itkFlipImageFilter.h>
 
 // SlicerRT includes
 #include <vtkSlicerRtCommon.h>
@@ -76,91 +75,61 @@ namespace
 int DoSetupDRR( int argc, char * argv[], Drr_options& options) throw(std::string)
 {
   PARSE_ARGS;
-/*
 
-plastimatch drr -A cpu \
-	--nrm "0.980282 -0.052336 0.190547" \
-	--vup "-0.0513744 -0.99863 -0.00998617" \
-	--sad 1000 --sid 1300 \
-	-r "2000 2000" \
-	-z "500 500" \
-	-c "922.5 969" \
-	-o "1.43469 -20.7674 -49.7378" \
-	-w "300 1546 180 1759" \
-	-e -i exact -P preprocess -O Out -t raw
-
-*/
-/*
-
-Input file: /tmp/Slicer-michel/inputVolumeFile.mha
-Output prefix: Out
-Output details prefix: 
-Output details file name: 
-plastimatch drr -A cpu \
-have normal: 1
-	--nrm "0.980282 -0.052336 0.190547" \
-	--vup "-0.0513744 -0.99863 -0.00998617" \
-	--sad 1000 --sid 1300 \
-	-r "2000 2000" \
-	-z "500 500" \
-have center: 1
-	-c "922.5 969" \
-	-o "1.43469 -20.7674 -49.7378" \
-have window: 1
-	-w "300 1546 180 1759" \
-	-e -i exact -P preprocess -t raw
-
-*/
-
-  // Single image mode
+  // Single image mode, no geometry
   options.have_angle_diff = false;
   options.num_angles = 1;
   options.start_angle = 0.;
+  options.geometry_only = 0;
 
   // Imager MUST be with image window and image center and image resolution
   options.have_image_center = 1;
-  options.have_image_window = 1;
-  options.image_resolution[0] = 2000;
-  options.image_resolution[1] = 2000;
-  options.detector_resolution[0] = 2000;
-  options.detector_resolution[1] = 2000;
-  options.image_size[0] = 500.;
-  options.image_size[1] = 500.;
-  options.image_window[0] = 0;
-  options.image_window[1] = 1999;
-  options.image_window[2] = 0;
-  options.image_window[3] = 1999;
-  options.image_center[0] = 999.; // columns
-  options.image_center[1] = 999.; // rows
-//  options.image_resolution[0] = options.image_window[1] - options.image_window[0] + 1;
-//  options.image_resolution[1] = options.image_window[3] - options.image_window[2] + 1;
-
-//  float imageCenterX = float(imageWindow[2]) + float(imageWindow[3] - imageWindow[2] - 1) / 2.f; // columns
-//  float imageCenterY = float(imageWindow[0]) + float(imageWindow[1] - imageWindow[0] - 1) / 2.f; // rows
-
+  options.have_image_window = useImageWindow;
+  options.detector_resolution[0] = imagerResolution[0];
+  options.detector_resolution[1] = imagerResolution[1];
+  options.image_size[0] = imagerResolution[0] * imagerSpacing[0];
+  options.image_size[1] = imagerResolution[1] * imagerSpacing[1];
+  if (useImageWindow)
+  {
+    options.image_window[0] = std::max<int>( 0, imageWindow[0]); // start column
+    options.image_window[1] = std::min<int>( imagerResolution[0] - 1, imageWindow[2]); // end column
+    options.image_window[2] = std::max<int>( 0, imageWindow[1]); // start row
+    options.image_window[3] = std::min<int>( imagerResolution[1] - 1, imageWindow[3]); // end row
+  }
+  else
+  {
+    options.image_window[0] = 0; // start column
+    options.image_window[1] = imagerResolution[0] - 1; // end column
+    options.image_window[2] = 0; // start row
+    options.image_window[3] = imagerResolution[1] - 1; // end row
+  }
+  options.image_center[0] = options.image_window[0] + (options.image_window[1] - options.image_window[0]) / 2.f; // column
+  options.image_center[1] = options.image_window[2] + (options.image_window[3] - options.image_window[2]) / 2.f; // row
+  options.image_resolution[0] = options.image_window[1] - options.image_window[0] + 1;
+  options.image_resolution[1] = options.image_window[3] - options.image_window[2] + 1;
 
   // geometry
   // vup and normal vectors
-  options.vup[0] = 0.0513744;
-  options.vup[1] = -0.99863;
-  options.vup[2] = -0.00998617;
+  options.vup[0] = viewUpVector[0];
+  options.vup[1] = viewUpVector[1];
+  options.vup[2] = viewUpVector[2];
 
   options.have_nrm = 1;
-  options.nrm[0] = 0.980282;
-  options.nrm[1] = -0.052336;
-  options.nrm[2] = 0.190547;
+  options.nrm[0] = normalVector[0];
+  options.nrm[1] = normalVector[1];
+  options.nrm[2] = normalVector[2];
 
   if (sourceImagerDistance < sourceAxisDistance)
   {
     throw std::string("SID is less than SAD");
   }
-  options.sad = 1000.;//sourceAxisDistance;
-  options.sid = 1300.;//sourceImagerDistance;
+  options.sad = sourceAxisDistance;
+  options.sid = sourceImagerDistance;
 
   // Isocenter DICOM (LPS) position
-  options.isocenter[0] = 1.43469;//isocenterPosition[0];
-  options.isocenter[1] = -20.7674;//isocenterPosition[1];
-  options.isocenter[2] = -49.7378;//isocenterPosition[2];
+  options.isocenter[0] = isocenterPosition[0];
+  options.isocenter[1] = isocenterPosition[1];
+  options.isocenter[2] = isocenterPosition[2];
 
   // intensity
   options.exponential_mapping = static_cast<int>(exponentialMapping);
@@ -169,8 +138,8 @@ have window: 1
   {
     throw std::string("Autoscale range is wrong");
   }
-  options.autoscale_range[0] = 0.;//autoscaleRange[0];
-  options.autoscale_range[1] = 255.;//autoscaleRange[1];
+  options.autoscale_range[0] = autoscaleRange[0];
+  options.autoscale_range[1] = autoscaleRange[1];
 
   // processing
   options.threading = threading_parse(threading);
@@ -222,76 +191,58 @@ have window: 1
     throw std::string("Wrong output file format");
   }
 
-  options.output_prefix = std::string("Out");
-
-/*
-
-  switch (this->AlgorithmReconstuction)
-  {
-    case AlgorithmReconstuctionType::EXACT:
-      command << "-i exact ";
-      break;
-    case AlgorithmReconstuctionType::UNIFORM:
-      command << "-i uniform ";
-      break;
-    default:
-      break;
-  }
-
-  switch (this->HUConversion)
-  {
-    case HounsfieldUnitsConversionType::NONE:
-      command << "-P none ";
-      break;
-    case HounsfieldUnitsConversionType::PREPROCESS:
-      command << "-P preprocess ";
-      break;
-    case HounsfieldUnitsConversionType::INLINE:
-      command << "-P inline ";
-      break;
-    default:
-      break;
-  }
-
-  command << "-O Out -t raw";
-*/
   return EXIT_SUCCESS;
 }
 
 template <typename TPixel>
-int DoIt( int argc, char * argv[], std::string& fileName, TPixel )
+int DoIt( int argc, char * argv[], Drr_options& options, TPixel ) throw(itk::ExceptionObject)
 {
   PARSE_ARGS;
 
-  typedef TPixel InputPixelType; // CT pixel type (short)
-//  typedef signed long int OutputPixelType; // Plastimatch DRR pixel type (long)
-
+  using InputPixelType = TPixel; // CT pixel type (short)
   const unsigned int Dimension = 3;
 
   // CT image type and reader
-  typedef itk::Image< InputPixelType, Dimension> InputImageType;
-  typedef itk::ImageFileReader<InputImageType> InputReaderType;
-
+  using InputImageType = itk::Image< InputPixelType, Dimension>;
+  using InputReaderType = itk::ImageFileReader<InputImageType>;
+  
   typename InputReaderType::Pointer inputReader = InputReaderType::New();
   inputReader->SetFileName( inputVolume.c_str() );
 
   // MetaImageHeader temporary writer
-  typedef itk::ImageFileWriter<InputImageType>  InputWriterType;
+  using InputWriterType = itk::ImageFileWriter<InputImageType>;
   typename InputWriterType::Pointer inputWriter = InputWriterType::New();
-  
+
+  // Input and output files options
+  std::string mhdFilename;
   std::size_t found = inputVolume.find_last_of("/\\");
-  std::string mhaFilename = "inputVolume.mha";
   if (found < inputVolume.size() - 1)
   {
-    mhaFilename = inputVolume.substr( 0, found + 1) + mhaFilename;
-    fileName = mhaFilename;
+    std::string tmpDir = inputVolume.substr( 0, found + 1);
+    options.input_file = tmpDir + "inputVolume.mha";
+    options.output_file = tmpDir + "outputVolume.raw";
+    mhdFilename = tmpDir + "outputVolume.mhd";
+
+    // setup meta file info for raw file
+    std::ofstream ofs(mhdFilename.c_str());
+    ofs << "NDims = " << Dimension << "\n";
+    ofs << "DimSize = " << options.image_resolution[0] << " " << options.image_resolution[1] << " 1\n"; // x (columns), y (rows), 1
+    float spacingColumns = options.image_size[0] / float(options.detector_resolution[0]);
+    float spacingRows = options.image_size[1] / float(options.detector_resolution[1]);
+    ofs << "ElementSpacing = " << spacingColumns << " " << spacingRows << " 1\n"; // x (columns), y (rows), 1
+    ofs << "Position = 0 0 0\n";
+    ofs << "BinaryData = True\n";
+    ofs << "ElementByteOrderMSB = False\n";
+    ofs << "ElementType = MET_LONG\n";
+    ofs << "ElementDataFile = outputVolume.raw\n";
+    ofs.close();
   }
   else if (found == inputVolume.size() or found == std::string::npos)
   {
-    return EXIT_FAILURE;
+    throw std::string("Unable to find directory name");
   }
-  
-  inputWriter->SetFileName(mhaFilename.c_str());
+
+  inputWriter->SetFileName(options.input_file.c_str());
   inputWriter->SetInput(inputReader->GetOutput());
   try
   {
@@ -301,21 +252,48 @@ int DoIt( int argc, char * argv[], std::string& fileName, TPixel )
   {
     throw;
   }
-//    typedef itk::Image<OutputPixelType, Dimension> OutputImageType;
-/*
-  typedef itk::SmoothingRecursiveGaussianImageFilter<
-    InputImageType, OutputImageType>  FilterType;
-  typename FilterType::Pointer filter = FilterType::New();
-  filter->SetInput( reader->GetOutput() );
-  filter->SetSigma( sigma );
 
-  typedef itk::ImageFileWriter<OutputImageType> WriterType;
+  // Compute DRR image if everything is OK
+  drr_compute(&options);
+
+  // Plastimatch DRR pixel type (long)
+  using PlmDrrPixelType = signed long int;
+  using PlmDrrImageType = itk::Image< PlmDrrPixelType, Dimension >;
+  using PlmDrrReaderType = itk::ImageFileReader< PlmDrrImageType >;
+
+  // read mhd file
+  typename PlmDrrReaderType::Pointer drrReader = PlmDrrReaderType::New();
+  drrReader->SetFileName(mhdFilename.c_str());
+
+  // rescale to autoscale range
+  using RescaleFilterType = itk::RescaleIntensityImageFilter< PlmDrrImageType, PlmDrrImageType >;
+  typename RescaleFilterType::Pointer rescale = RescaleFilterType::New();
+  rescale->SetOutputMinimum(options.autoscale_range[0]);
+  rescale->SetOutputMaximum(options.autoscale_range[1]);
+  rescale->SetInput(drrReader->GetOutput());
+
+  // invert (optional?)
+  using InvertFilterType = itk::InvertIntensityImageFilter< PlmDrrImageType, PlmDrrImageType >;
+  InvertFilterType::Pointer invert = InvertFilterType::New();
+  invert->SetInput(rescale->GetOutput());
+  invert->SetMaximum(options.autoscale_range[0]);
+
+  // write data into Slicer
+  using WriterType = itk::ImageFileWriter< PlmDrrImageType >;
   typename WriterType::Pointer writer = WriterType::New();
   writer->SetFileName( outputVolume.c_str() );
-  writer->SetInput( filter->GetOutput() );
-  writer->SetUseCompression(1);
-  writer->Update();
-*/
+  writer->SetInput( invert->GetOutput() );
+//  writer->SetUseCompression(1);
+
+  try
+  {
+    writer->Update();
+  }
+  catch ( itk::ExceptionObject & excep )
+  {
+    throw;
+  }
+
   return EXIT_SUCCESS;
 }
 
@@ -328,13 +306,11 @@ int main( int argc, char * argv[] )
   itk::ImageIOBase::IOPixelType     pixelType;
   itk::ImageIOBase::IOComponentType componentType;
 
-  int res = EXIT_FAILURE;
-  Drr_options drrOptions;
-  std::string mhaFileName;
   try
   {
-    // Since there are no conditional flags, EVERY options must be defined
-    // and checked.
+    // Since there are no conditional flags, EVERY options must be 
+    // explicitly defined and checked.
+    Drr_options drrOptions;
     DoSetupDRR( argc, argv, drrOptions);
 
     itk::GetImageType(inputVolume, pixelType, componentType);
@@ -344,34 +320,34 @@ int main( int argc, char * argv[] )
     switch( componentType )
     {
     case itk::ImageIOBase::UCHAR:
-      res = DoIt( argc, argv, mhaFileName, static_cast<unsigned char>(0) );
+      return DoIt( argc, argv, drrOptions, static_cast<unsigned char>(0) );
       break;
     case itk::ImageIOBase::CHAR:
-      res = DoIt( argc, argv, mhaFileName, static_cast<signed char>(0) );
+      return DoIt( argc, argv, drrOptions, static_cast<signed char>(0) );
       break;
     case itk::ImageIOBase::USHORT:
-      res = DoIt( argc, argv, mhaFileName, static_cast<unsigned short>(0) );
+      return DoIt( argc, argv, drrOptions, static_cast<unsigned short>(0) );
       break;
-    case itk::ImageIOBase::SHORT:
-      res = DoIt( argc, argv, mhaFileName, static_cast<short>(0) );
+    case itk::ImageIOBase::SHORT: // CT data, everything else is a mistake
+      return DoIt( argc, argv, drrOptions, static_cast<short>(0) );
       break;
     case itk::ImageIOBase::UINT:
-      res = DoIt( argc, argv, mhaFileName, static_cast<unsigned int>(0) );
+      return DoIt( argc, argv, drrOptions, static_cast<unsigned int>(0) );
       break;
     case itk::ImageIOBase::INT:
-      res = DoIt( argc, argv, mhaFileName, static_cast<int>(0) );
+      return DoIt( argc, argv, drrOptions, static_cast<int>(0) );
       break;
     case itk::ImageIOBase::ULONG:
-      res = DoIt( argc, argv, mhaFileName, static_cast<unsigned long>(0) );
+      return DoIt( argc, argv, drrOptions, static_cast<unsigned long>(0) );
       break;
     case itk::ImageIOBase::LONG:
-      res = DoIt( argc, argv, mhaFileName, static_cast<long>(0) );
+      return DoIt( argc, argv, drrOptions, static_cast<long>(0) );
       break;
     case itk::ImageIOBase::FLOAT:
-      res = DoIt( argc, argv, mhaFileName, static_cast<float>(0) );
+      return DoIt( argc, argv, drrOptions, static_cast<float>(0) );
       break;
     case itk::ImageIOBase::DOUBLE:
-      res = DoIt( argc, argv, mhaFileName, static_cast<double>(0) );
+      return DoIt( argc, argv, drrOptions, static_cast<double>(0) );
       break;
     case itk::ImageIOBase::UNKNOWNCOMPONENTTYPE:
     default:
@@ -384,7 +360,7 @@ int main( int argc, char * argv[] )
   }
   catch( itk::ExceptionObject & excep )
   {
-    std::cerr << argv[0] << ": exception caught !" << std::endl;
+    std::cerr << argv[0] << ": ITK exception caught !" << std::endl;
     std::cerr << excep << std::endl;
     return EXIT_FAILURE;
   }
@@ -402,15 +378,9 @@ int main( int argc, char * argv[] )
   }
   catch( ... )
   {
-    std::cerr << "Desaster, unknown exception caught!" << std::endl;
+    std::cerr << "Disaster, unknown exception caught!" << std::endl;
     return EXIT_FAILURE;
   }
 
-  // Compute DRR image if mha file name is valid
-  if (res == EXIT_SUCCESS && !mhaFileName.empty())
-  {
-    drrOptions.input_file = mhaFileName;
-    drr_compute(&drrOptions);
-  }
   return EXIT_SUCCESS;
 }
