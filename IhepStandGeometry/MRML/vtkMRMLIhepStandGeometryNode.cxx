137e6dfdec17d0a64e165ba8855323771f7f99cb
Mikhail Polkovnikov <pmkclnl@yandex.ru>
2020-12-03 07:39:43 +0300

    IhepStandGeometry node is added

---
+++ IhepStandGeometry/MRML/vtkMRMLIhepStandGeometryNode.cxx
@@ -0,0 +1,156 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Portions (c) Copyright Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+==============================================================================*/
+
+
+// Beams includes
+#include <vtkMRMLRTBeamNode.h>
+
+// MRML includes
+#include <vtkMRMLScene.h>
+#include <vtkMRMLLinearTransformNode.h>
+
+// VTK includes
+#include <vtkObjectFactory.h>
+#include <vtkSmartPointer.h>
+#include <vtkTransform.h>
+#include <vtkMatrix4x4.h>
+
+#include "vtkMRMLIhepStandGeometryNode.h"
+
+//------------------------------------------------------------------------------
+namespace
+{
+
+const char* BEAM_REFERENCE_ROLE = "beamRef";
+
+} // namespace
+
+//------------------------------------------------------------------------------
+vtkMRMLNodeNewMacro(vtkMRMLIhepStandGeometryNode);
+
+//----------------------------------------------------------------------------
+vtkMRMLIhepStandGeometryNode::vtkMRMLIhepStandGeometryNode()
+{
+  PatientSupportRotationAngle = 0.; // theta_s
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLIhepStandGeometryNode::~vtkMRMLIhepStandGeometryNode()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLIhepStandGeometryNode::WriteXML(ostream& of, int nIndent)
+{
+  Superclass::WriteXML(of, nIndent);
+
+  // Write all MRML node attributes into output stream
+  vtkMRMLWriteXMLBeginMacro(of);
+  vtkMRMLWriteXMLFloatMacro(PatientSupportRotationAngle, PatientSupportRotationAngle);
+  // add new parameters here
+  vtkMRMLWriteXMLEndMacro();
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLIhepStandGeometryNode::ReadXMLAttributes(const char** atts)
+{
+  int disabledModify = this->StartModify();
+
+  vtkMRMLNode::ReadXMLAttributes(atts);
+
+  vtkMRMLReadXMLBeginMacro(atts);
+  vtkMRMLReadXMLFloatMacro(PatientSupportRotationAngle, PatientSupportRotationAngle);
+  // add new parameters here
+  vtkMRMLReadXMLEndMacro();
+
+  this->EndModify(disabledModify);
+
+  // Note: ReportString is not read from XML, it is a strictly temporary value
+}
+
+//----------------------------------------------------------------------------
+// Copy the node's attributes to this object.
+void vtkMRMLIhepStandGeometryNode::Copy(vtkMRMLNode *anode)
+{
+  int disabledModify = this->StartModify();
+
+  Superclass::Copy(anode);
+
+  vtkMRMLIhepStandGeometryNode* node = vtkMRMLIhepStandGeometryNode::SafeDownCast(anode);
+  if (!node)
+  {
+    return;
+  }
+
+  // Copy parameters
+  this->DisableModifiedEventOn();
+  vtkMRMLCopyBeginMacro(node);
+  vtkMRMLCopyFloatMacro(PatientSupportRotationAngle);
+  // add new parameters here
+  vtkMRMLCopyEndMacro(); 
+
+  this->EndModify(disabledModify);
+
+  this->InvokePendingModifiedEvent();
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLIhepStandGeometryNode::CopyContent(vtkMRMLNode *anode, bool deepCopy/*=true*/)
+{
+  MRMLNodeModifyBlocker blocker(this);
+  Superclass::CopyContent(anode, deepCopy);
+
+  vtkMRMLIhepStandGeometryNode* node = vtkMRMLIhepStandGeometryNode::SafeDownCast(anode);
+  if (!node)
+  {
+    return;
+  }
+
+  vtkMRMLCopyBeginMacro(node);
+  vtkMRMLCopyFloatMacro(PatientSupportRotationAngle);
+  // add new parameters here
+  vtkMRMLCopyEndMacro();
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLIhepStandGeometryNode::PrintSelf(ostream& os, vtkIndent indent)
+{
+  Superclass::PrintSelf(os,indent);
+
+  vtkMRMLPrintBeginMacro(os, indent);
+  vtkMRMLPrintFloatMacro(PatientSupportRotationAngle);
+  // add new parameters here
+  vtkMRMLPrintEndMacro(); 
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLRTBeamNode* vtkMRMLIhepStandGeometryNode::GetBeamNode()
+{
+  return vtkMRMLRTBeamNode::SafeDownCast( this->GetNodeReference(BEAM_REFERENCE_ROLE) );
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLIhepStandGeometryNode::SetAndObserveBeamNode(vtkMRMLRTBeamNode* node)
+{
+  if (node && this->Scene != node->GetScene())
+  {
+    vtkErrorMacro("SetAndObserveBeamNode: Cannot set reference, the referenced and referencing node are not in the same scene");
+    return;
+  }
+
+  this->SetNodeReferenceID(BEAM_REFERENCE_ROLE, (node ? node->GetID() : nullptr));
+}
---
+++ IhepStandGeometry/MRML/vtkMRMLIhepStandGeometryNode.h
@@ -0,0 +1,82 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Portions (c) Copyright Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+==============================================================================*/
+
+#ifndef __vtkMRMLIhepStandGeometryNode_h
+#define __vtkMRMLIhepStandGeometryNode_h
+
+// Beams includes
+#include "vtkSlicerIhepStandGeometryModuleMRMLExport.h"
+
+// MRML includes
+#include <vtkMRML.h>
+#include <vtkMRMLNode.h>
+
+class vtkMRMLLinearTransformNode;
+class vtkMRMLRTBeamNode;
+class vtkMRMLMarkupsClosedCurveNode;
+class vtkMRMLMarkupsFiducialNode;
+class vtkMRMLMarkupsLineNode;
+
+/// \ingroup SlicerRt_QtModules_IhepStandGeometry
+
+class VTK_SLICER_IHEPSTANDGEOMETRY_MODULE_MRML_EXPORT vtkMRMLIhepStandGeometryNode : public vtkMRMLNode
+{
+public:
+
+  static vtkMRMLIhepStandGeometryNode *New();
+  vtkTypeMacro(vtkMRMLIhepStandGeometryNode,vtkMRMLNode);
+  void PrintSelf(ostream& os, vtkIndent indent) override;
+
+  /// Create instance of a GAD node. 
+  vtkMRMLNode* CreateNodeInstance() override;
+
+  /// Set node attributes from name/value pairs 
+  void ReadXMLAttributes(const char** atts) override;
+
+  /// Write this node's information to a MRML file in XML format. 
+  void WriteXML(ostream& of, int indent) override;
+
+  /// Copy the node's attributes to this object 
+  void Copy(vtkMRMLNode *node) override;
+
+  /// Copy node content (excludes basic data, such a name and node reference)
+  vtkMRMLCopyContentMacro(vtkMRMLIhepStandGeometryNode);
+
+  /// Get unique node XML tag name
+  const char* GetNodeTagName() override { return "IhepStandGeometry"; };
+
+public:
+  /// Get beam node
+  vtkMRMLRTBeamNode* GetBeamNode();
+  /// Set and observe beam node. This updates Normal and View-Up vectors.
+  void SetAndObserveBeamNode(vtkMRMLRTBeamNode* node);
+
+  /// Get/Set patient support rotation angle
+  vtkSetMacro(PatientSupportRotationAngle, double);
+  vtkGetMacro(PatientSupportRotationAngle, double);
+
+protected:
+  vtkMRMLIhepStandGeometryNode();
+  ~vtkMRMLIhepStandGeometryNode();
+  vtkMRMLIhepStandGeometryNode(const vtkMRMLIhepStandGeometryNode&);
+  void operator=(const vtkMRMLIhepStandGeometryNode&);
+
+protected:
+  double PatientSupportRotationAngle;
+};
+
+#endif
