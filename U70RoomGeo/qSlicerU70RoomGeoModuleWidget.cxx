053b424f8731147a72e08ddaf3f7ffc86f5d2f25
Mikhail Polkovnikov <pmkclnl@yandex.ru>
2026-08-10 13:54:46 +0300

    ENH: U-70 treatment room geometry module (cont.)

--- U70RoomGeo/qSlicerU70RoomGeoModuleWidget.cxx
+++ U70RoomGeo/qSlicerU70RoomGeoModuleWidget.cxx
@@ -17,12 +17,16 @@
 
 // Qt includes
 #include <QDebug>
+#include <QFileDialog>
+#include <QMessageBox>
 
 // Slicer includes
 #include "qSlicerU70RoomGeoModuleWidget.h"
 #include "ui_qSlicerU70RoomGeoModuleWidget.h"
 
+// MRML includes
 #include <vtkMRMLScene.h>
+#include <vtkMRMLSubjectHierarchyNode.h>
 
 // U70RoomGeo logic and nodes
 #include <vtkMRMLU70RoomGeoNode.h>
@@ -37,10 +41,17 @@ protected:
   qSlicerU70RoomGeoModuleWidget* const q_ptr;
 public:
   qSlicerU70RoomGeoModuleWidgetPrivate(qSlicerU70RoomGeoModuleWidget& object);
-  ~qSlicerU70RoomGeoModuleWidgetPrivate()  = default;
-  vtkSmartPointer<vtkSlicerU70RoomGeoLogic> logic() const;
+  ~qSlicerU70RoomGeoModuleWidgetPrivate() = default;
 
-  bool ModuleWindowInitialized;
+  vtkSmartPointer<vtkSlicerU70RoomGeoLogic> logic() const;
+  vtkSlicerChannel26Cabin3RobotsTransformLogic* channel26RobotsLogic() const;
+
+  QString getTreatmentRoomGeometryFile() const;
+
+  /// U70 treatment room geometry MRML node containing shown parameters
+  vtkSmartPointer<vtkMRMLU70RoomGeoNode> ParameterNode;
+
+  bool ModuleWindowInitialized{ false };
 };
 
 //-----------------------------------------------------------------------------
@@ -58,6 +69,32 @@ vtkSmartPointer<vtkSlicerU70RoomGeoLogic> qSlicerU70RoomGeoModuleWidgetPrivate::
 {
   Q_Q(const qSlicerU70RoomGeoModuleWidget);
   return vtkSlicerU70RoomGeoLogic::SafeDownCast(q->logic());
+}
+
+//-----------------------------------------------------------------------------
+vtkSlicerChannel26Cabin3RobotsTransformLogic* qSlicerU70RoomGeoModuleWidgetPrivate::channel26RobotsLogic() const
+{
+  Q_Q(const qSlicerU70RoomGeoModuleWidget);
+  vtkSlicerU70RoomGeoLogic* logic = vtkSlicerU70RoomGeoLogic::SafeDownCast(q->logic());
+  return (logic) ? logic->GetChannel26RobotsTransformLogic() : nullptr;
+}
+
+QString qSlicerU70RoomGeoModuleWidgetPrivate::getTreatmentRoomGeometryFile() const
+{
+  QRadioButton* rButton = qobject_cast< QRadioButton* >(this->ButtonGroup_TreatmentRoomType->checkedButton());
+  if (rButton && rButton == this->RadioButton_Cabin1)
+  {
+    return "Channel26Cabin1Geometry";
+  }
+  else if (rButton && rButton == this->RadioButton_Cabin2)
+  {
+    return "Channel26Cabin2Geometry";
+  }
+  else if (rButton && rButton == this->RadioButton_Cabin2)
+  {
+    return "Channel26Cabin3Geometry";
+  }
+  return QString();
 }
 
 //-----------------------------------------------------------------------------
@@ -89,6 +126,243 @@ void qSlicerU70RoomGeoModuleWidget::setup()
 void qSlicerU70RoomGeoModuleWidget::onLoadTreatmentRoomButtonClicked()
 {
   Q_D(qSlicerU70RoomGeoModuleWidget);
+
+  vtkMRMLScene* scene = this->mrmlScene();
+  if (!scene)
+  {
+    qCritical() << Q_FUNC_INFO << ": Invalid scene";
+    return;
+  }
+
+  if (!d->ParameterNode)
+  {
+    return;
+  }
+
+  using SysCoord = vtkSlicerChannel26Cabin3RobotsTransformLogic::CoordinateSystemIdentifier;
+  // Get treatment machine descriptor file path
+  QString treatmentMachineType = d->getTreatmentRoomGeometryFile();
+  QString descriptorFilePath;
+  if (!treatmentMachineType.compare("FromFile"))
+  {
+    // Ask user for descriptor JSON file if load from file option is selected
+    descriptorFilePath = QFileDialog::getOpenFileName( this, "Select treatment machine descriptor JSON file...",
+      QString(), "Json files (*.json);; All files (*)" ); 
+  }
+  else //TODO: Currently support two default types in addition to loading file. Need to rethink the module
+  {
+    std::string treatMachType = treatmentMachineType.toStdString();
+    d->ParameterNode->SetTreatmentMachineType(treatMachType.c_str());
+    QString relativeFilePath = QString("%1/%2.json").arg(treatmentMachineType).arg(treatmentMachineType);
+    descriptorFilePath = QDir(d->logic()->GetModuleShareDirectory().c_str()).filePath(relativeFilePath);
+  }
+  std::string descFilePath = descriptorFilePath.toStdString();
+  d->ParameterNode->SetTreatmentMachineDescriptorFilePath(descFilePath.c_str());
+
+  // Set treatment machine dependent properties  //TODO: Use degrees of freedom from JSON
+  if (!treatmentMachineType.compare("Channel26Cabin3Geometry"))
+  {
+    qDebug() << Q_FUNC_INFO << "Channel-26 Cabin-3";
+    d->ParameterNode->SetTreatmentMachineType("Channel26Cabin3Geometry");
+  }
+  else if (!treatmentMachineType.compare("Channel26Cabin2Geometry"))
+  {
+    qDebug() << Q_FUNC_INFO << "Channel-26 Cabin-2";
+    d->ParameterNode->SetTreatmentMachineType("Channel26Cabin2Geometry");
+  }
+  else if (!treatmentMachineType.compare("Channel26Cabin1Geometry"))
+  {
+    qDebug() << Q_FUNC_INFO << "Channel-26 Cabin-1";
+    d->ParameterNode->SetTreatmentMachineType("Channel26Cabin1Geometry");
+  }
+
+  // Check if there is a machine already loaded and ask user what to do if so
+  vtkMRMLSubjectHierarchyNode* shNode = this->mrmlScene()->GetSubjectHierarchyNode();
+  std::vector<vtkIdType> allItemIDs;
+  shNode->GetItemChildren(shNode->GetSceneItemID(), allItemIDs, true);
+  std::vector<vtkIdType> machineFolderItemIDs;
+  std::vector<vtkIdType>::iterator itemIt;
+  for (itemIt=allItemIDs.begin(); itemIt!=allItemIDs.end(); ++itemIt)
+  {
+    std::string machineDescriptorFilePath = shNode->GetItemAttribute(*itemIt, "TreatmentMachineDescriptorFilePath");
+    if (!machineDescriptorFilePath.compare(descriptorFilePath.toUtf8().constData()))
+    {
+      QMessageBox::warning(this, tr("Machine already loaded"), tr("This treatment machine is already loaded."));
+      return;
+    }
+    if (!machineDescriptorFilePath.empty())
+    {
+      machineFolderItemIDs.push_back(*itemIt);
+    }
+  }
+  // Create a new vtkMRMLChannel26GeometryNode;
+  // Set and observe created node in U70RoomGeo node
+  if (!scene->GetFirstNodeByClass("vtkMRMLChannel26GeometryNode"))
+  {
+    vtkNew<vtkMRMLChannel26GeometryNode> channel26GeometryNode;
+    channel26GeometryNode->SetName("Channel26Geometry");
+    channel26GeometryNode->SetHideFromEditors(0);
+    scene->AddNode(channel26GeometryNode);
+    d->ParameterNode->SetAndObserveChannel26GeometryNode(channel26GeometryNode.GetPointer());
+  }
+  else
+  {
+    vtkMRMLChannel26GeometryNode* channel26GeometryNode = vtkMRMLChannel26GeometryNode::SafeDownCast(
+      scene->GetFirstNodeByClass("vtkMRMLChannel26GeometryNode"));
+    d->ParameterNode->SetAndObserveChannel26GeometryNode(channel26GeometryNode);
+  }
+  d->ParameterNode->SetTreatmentMachineDescriptorFilePath(descriptorFilePath.toUtf8().constData());
+
+  if (machineFolderItemIDs.size() > 0)
+  {
+    ctkMessageBox* existingMachineMsgBox = new ctkMessageBox(this);
+    existingMachineMsgBox->setWindowTitle(tr("Other machines loaded"));
+    existingMachineMsgBox->setText(tr("There is another treatment machine loaded in the scene. Would you like to hide or delete it?"));
+
+    existingMachineMsgBox->addButton(tr("Hide"), QMessageBox::AcceptRole);
+    existingMachineMsgBox->addButton(tr("Delete"), QMessageBox::DestructiveRole);
+    existingMachineMsgBox->addButton(tr("No action"), QMessageBox::RejectRole);
+
+    existingMachineMsgBox->setDontShowAgainVisible(true);
+    existingMachineMsgBox->setDontShowAgainSettingsKey("SlicerRT/DontAskOnMultipleTreatmentMachines");
+    existingMachineMsgBox->setIcon(QMessageBox::Question);
+    existingMachineMsgBox->exec();
+    int resultCode = existingMachineMsgBox->buttonRole(existingMachineMsgBox->clickedButton());
+    if (resultCode == QMessageBox::AcceptRole)
+    {
+      qSlicerSubjectHierarchyFolderPlugin* folderPlugin = qobject_cast<qSlicerSubjectHierarchyFolderPlugin*>(
+        qSlicerSubjectHierarchyPluginHandler::instance()->pluginByName("Folder") );
+      for (itemIt=machineFolderItemIDs.begin(); itemIt!=machineFolderItemIDs.end(); ++itemIt)
+      {
+        folderPlugin->setDisplayVisibility(*itemIt, false);
+      }
+    }
+    else if (resultCode == QMessageBox::DestructiveRole)
+    {
+      for (itemIt=machineFolderItemIDs.begin(); itemIt!=machineFolderItemIDs.end(); ++itemIt)
+      {
+        shNode->RemoveItem(*itemIt);
+      }
+    }
+  }
+  QApplication::setOverrideCursor(Qt::WaitCursor);
+  // Load and setup models
+  std::vector< SysCoord > loadedParts;
+  vtkMRMLChannel26GeometryNode* channel26GeometryNode = d->ParameterNode->GetChannel26GeometryNode();
+  if (channel26GeometryNode)
+  {
+    d->getLayoutManager()->pauseRender();
+    loadedParts = d->logic()->LoadTreatmentMachineComponents(d->ParameterNode);
+    d->getLayoutManager()->resumeRender();
+  }
+  // TODO: Unable collision detection later when models will be OK
+/*
+  // Warn the user if collision detection is disabled for certain part pairs
+  QString disabledCollisionDetectionMessage(
+    tr("Collision detection for the following part pairs may take very long due to high triangle numbers:\n\n"));
+  bool collisionDetectionDisabled = false;
+  if (d->logic()->GetTableTopFixedReferenceCollisionDetection()->GetInputData(0) == nullptr)
+  {
+    disabledCollisionDetectionMessage.append("TableTop-FixedReference\n");
+    collisionDetectionDisabled = true;
+  }
+  if (d->logic()->GetTableTopElbowCollisionDetection()->GetInputData(0) == nullptr)
+  {
+    disabledCollisionDetectionMessage.append("TableTop-Elbow\n");
+    collisionDetectionDisabled = true;
+  }
+  if (d->logic()->GetTableTopShoulderCollisionDetection()->GetInputData(0) == nullptr)
+  {
+    disabledCollisionDetectionMessage.append("TableTop-Shoulder\n");
+    collisionDetectionDisabled = true;
+  }
+  if (d->logic()->GetTableTopBaseRotationCollisionDetection()->GetInputData(0) == nullptr)
+  {
+    disabledCollisionDetectionMessage.append("TableTop-BaseRotation\n");
+    collisionDetectionDisabled = true;
+  }
+  if (d->logic()->GetTableTopBaseFixedCollisionDetection()->GetInputData(0) == nullptr)
+  {
+    disabledCollisionDetectionMessage.append("TableTop-BaseFixed\n");
+    collisionDetectionDisabled = true;
+  }
+  disabledCollisionDetectionMessage.append(tr("\nWhat would you like to do?"));
+  if (collisionDetectionDisabled)
+  {
+    ctkMessageBox* existingMachineMsgBox = new ctkMessageBox(this);
+    existingMachineMsgBox->setWindowTitle(tr("Collision detection might take too long"));
+    existingMachineMsgBox->setText(disabledCollisionDetectionMessage);
+    existingMachineMsgBox->addButton(tr("Disable on these part pairs"), QMessageBox::AcceptRole);
+    existingMachineMsgBox->addButton(tr("Calculate anyway"), QMessageBox::RejectRole);
+    existingMachineMsgBox->setIcon(QMessageBox::Warning);
+    existingMachineMsgBox->exec();
+    int resultCode = existingMachineMsgBox->buttonRole(existingMachineMsgBox->clickedButton());
+    if (resultCode == QMessageBox::RejectRole)
+    {
+      // Set up treatment machine models again but make sure collision detection is not disabled between any parts
+      d->logic()->SetupTreatmentMachineModels(d->ParameterNode, true);
+    }
+  }
+*/
+
+  // Enable treatment machine geometry controls
+  d->PushButton_UnloadModels->setEnabled(true);
+  d->PushButton_LoadModels->setEnabled(false);
+
+  // Reset camera
+/*
+  qSlicerApplication* slicerApplication = qSlicerApplication::application();
+  qSlicerLayoutManager* layoutManager = slicerApplication->layoutManager();
+  qMRMLThreeDView* threeDView = layoutManager->threeDWidget(0)->threeDView();
+  threeDView->resetCamera();
+*/
+
+  vtkSlicerChannel26Cabin3RobotsTransformLogic* channel26Logic = d->channel26RobotsLogic();
+  if (channel26Logic && channel26GeometryNode)
+  {
+    d->getLayoutManager()->pauseRender();
+    channel26Logic->ResetToInitialPositions();
+    channel26Logic->UpdateTransformsHierarchy(channel26GeometryNode, SysCoord::Patient);
+
+    d->getLayoutManager()->resumeRender();
+  }
+  // Update channel-26 geometry node
+  channel26GeometryNode->Modified();
+
+  // Cabin-3 FixedReference beam
+//  vtkMRMLRTChannel26Cabin3BeamNode* cabin3BeamNode = d->logic()->CreateCabin3BeamPlanAndNode(d->ParameterNode);
+//  d->MRMLNodeComboBox_FixedReferenceBeam->setCurrentNode(cabin3BeamNode);
+
+  /// Setup Markups fixed beam axis and fixed isocenter
+//  vtkMRMLMarkupsLineNode* beamAxisLineNode = d->logic()->CreateCabin3BeamAxisLineNode(d->ParameterNode);
+//  vtkMRMLMarkupsFiducialNode* fixedIsocenterNode = d->logic()->CreateCabin3IsocenterFiducialNode(d->ParameterNode);
+//  Q_UNUSED(beamAxisLineNode);
+//  Q_UNUSED(fixedIsocenterNode);
+  
+  // C-arm x-ray beam and DRR computation node
+//  vtkMRMLRTCarmBeamNode* xrayNode = d->logic()->CreateCarmXrayPlanAndNode(d->ParameterNode);
+//  d->MRMLNodeComboBox_CarmXrayBeam->setCurrentNode(xrayNode);
+//  vtkMRMLDrrImageComputationNode* drrNode = d->logic()->CreateCarmXrayDrrNode(d->ParameterNode);
+//  drrNode->SetAndObserveBeamNode(xrayNode);
+//  d->ParameterNode->SetAndObserveDrrComputationNode(drrNode);
+
+  // set DRR node to children widgets
+//  d->CarmXrayBeamWidget->setDrrImageComputationNode(drrNode);
+//  d->CollapsibleButton_CarmRtImageDrrRegistration->setEnabled(drrNode ? true : false);
+/*
+  // Hide controls that do not have corresponding parts loaded
+  bool imagingPanelsLoaded = (std::find(loadedParts.begin(), loadedParts.end(), vtkSlicerRoomsEyeViewModuleLogic::ImagingPanelLeft) != loadedParts.end() ||
+      std::find(loadedParts.begin(), loadedParts.end(), vtkSlicerRoomsEyeViewModuleLogic::ImagingPanelRight) != loadedParts.end());
+  d->labelImagingPanel->setVisible(imagingPanelsLoaded);
+  d->ImagingPanelMovementSlider->setVisible(imagingPanelsLoaded);
+
+  // Set orientation marker
+  //TODO: Add new option 'Treatment room' to orientation marker choices and merged model with actual colors (surface scalars?)
+  //vtkMRMLViewNode* viewNode = threeDView->mrmlViewNode();
+  //viewNode->SetOrientationMarkerHumanModelNodeID(this->mrmlScene()->GetFirstNodeByName("EBRTOrientationMarkerModel")->GetID());
+*/
+//  d->ParameterNode->Modified();
+  QApplication::restoreOverrideCursor();
 }
 
 //-----------------------------------------------------------------------------
@@ -126,7 +400,26 @@ void qSlicerU70RoomGeoModuleWidget::setMRMLScene(vtkMRMLScene* scene)
 //-----------------------------------------------------------------------------
 void qSlicerU70RoomGeoModuleWidget::setParameterNode(vtkMRMLNode* node)
 {
-  vtkMRMLU70RoomGeoNode* chanGeoNode = vtkMRMLU70RoomGeoNode::SafeDownCast(node);
+  Q_D(qSlicerU70RoomGeoModuleWidget);
+
+  vtkMRMLU70RoomGeoNode* parameterNode = vtkMRMLU70RoomGeoNode::SafeDownCast(node);
+
+  // Each time the node is modified, the UI widgets are updated
+  qvtkReconnect( d->ParameterNode, parameterNode, vtkCommand::ModifiedEvent, 
+    this, SLOT( updateWidgetFromMRML() ) );
+
+  d->ParameterNode = parameterNode;
+
+  // Set parameter node to children (FixedBeamAxis, CarmXrayBeamWidget) widgets
+//  d->FixedBeamAxisWidget->setParameterNode(d->ParameterNode);
+//  d->CarmXrayBeamWidget->setParameterNode(d->ParameterNode);
+
+  // Set selected MRML nodes in comboboxes in the parameter set if it was nullptr there
+  // (then in the meantime the comboboxes selected the first one from the scene and we have to set that)
+  if (d->ParameterNode)
+  {
+  }
+  this->updateWidgetFromMRML();
 }
 
 //-----------------------------------------------------------------------------
@@ -142,6 +435,12 @@ void qSlicerU70RoomGeoModuleWidget::onSceneClosedEvent()
 //-----------------------------------------------------------------------------
 void qSlicerU70RoomGeoModuleWidget::updateWidgetFromMRML()
 {
+  Q_D(qSlicerRoomsEyeViewModuleWidget);
+
+  if (d->ParameterNode && this->mrmlScene())
+  {
+    qDebug() << Q_FUNC_INFO << "Parameter node and scene are OK!";
+  }
 }
 
 //-----------------------------------------------------------------------------
@@ -169,9 +468,25 @@ void qSlicerU70RoomGeoModuleWidget::onEnter()
     qCritical() << Q_FUNC_INFO << ": Invalid logic";
     return;
   }
-
   // Select or create parameter node
-  this->setMRMLScene(this->mrmlScene());
+  vtkMRMLU70RoomGeoNode* parameterNode = nullptr; 
+  // Try to find one in the scene
+  if (vtkMRMLNode* node = this->mrmlScene()->GetFirstNodeByClass("vtkMRMLU70RoomGeoNode"))
+  {
+    parameterNode = vtkMRMLU70RoomGeoNode::SafeDownCast(node);
+  }
 
+  if (parameterNode)
+  {
+    this->setParameterNode(parameterNode);
+  }
+
+  // Set logics to childred widgets
+//  d->CarmXrayBeamWidget->setMRMLScene(this->mrmlScene());
+//  d->CarmXrayBeamWidget->setPatientPositioningLogic(d->logic());
+
+  // All required data for GUI is initiated
+  this->updateWidgetFromMRML();
+  
   d->ModuleWindowInitialized = true;
 }
